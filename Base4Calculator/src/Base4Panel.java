7d49cee42566925233932b4d61ac75c55ac32cff
zhi <zhi@DESKTOP-16ISM72.fios-router.home>
2016-11-18 23:14:31 -0500

    ChangeListener Conditions/logic completed. need to edit calc/logic

--- Base4Calculator/src/Base4Panel.java
+++ Base4Calculator/src/Base4Panel.java
@@ -8,10 +8,9 @@ import java.util.ArrayList;
 
 public class Base4Panel extends JPanel {
 	private Base4Calc calc; // this object will actually do the calculating work
-//	private ArrayList<JButton> num = new ArrayList<JButton>(); //
+	
 //	private ArrayList<JButton> op = new ArrayList<JButton>();// 
-	
-	
+
 	private JButton zero;
 	private JButton one;
 	private JButton two; 
@@ -27,7 +26,9 @@ public class Base4Panel extends JPanel {
 	private JButton twelve;
 	private JButton thrtn;
 	private JButton fourtn;
-	private JButton fiftn;
+	private JButton fiftn;	
+	private ArrayList<JButton> num;
+	
 	
 	private JSlider slider;
 	private JTextField textField;
@@ -58,41 +59,8 @@ public class Base4Panel extends JPanel {
 		ActionListener clr = new freshListener();
 		
 		zero = new JButton("0"); 
-		zero.addActionListener(digit);
 		add(zero, "cell 3 3,alignx left,aligny center");
 				
-		slider = new JSlider();
-		slider.setMinorTickSpacing(1);
-		slider.setSnapToTicks(true);
-		slider.setPaintLabels(true);
-		slider.setMajorTickSpacing(2);
-		slider.setMinimum(2);
-		slider.setMaximum(16);
-			slider.addChangeListener(new ChangeListener() {
-			     public void stateChanged(ChangeEvent e) {
-			    JSlider source = (JSlider)e.getSource();
-			      if (!source.getValueIsAdjusting())
-			      {
-			    	//recall button de/activation needs to go here as well 
-			      if (slider.getValue()==2)
-			    	  calc.setBase(2);
-			      else if(slider.getValue()==3)
-			      calc.setBase(3);
-			      else if(slider.getValue()==4)
-			      {	 calc.setBase(4); 
-			      System.out.print("calculating in b4");
-			      }
-			     			      
-			      else if(slider.getValue()==16)
-						 calc.setBase(16);
-			      }
-			     } 
-			    });
-				
-				
-				
-		slider.setOrientation(SwingConstants.VERTICAL);
-		add(slider, "cell 0 2 2 5");
 		
 		textField = new JTextField();
 		add(textField, "cell 3 2 5 1,growx");
@@ -102,71 +70,79 @@ public class Base4Panel extends JPanel {
 		
 	//	Creation of numeric buttons
 		one = new JButton("1");
-		one.addActionListener(digit);
 		add(one, "cell 4 3,alignx left,aligny center");
-	
-		
-		
+			
 		two = new JButton("2"); 
-		two.addActionListener(digit);
 		add(two, "cell 5 3,alignx left,aligny center");
 		
-		
 		three = new JButton("3"); 
-		three.addActionListener(digit);	
 		add(three, "cell 6 3,alignx left,aligny center");
-		
-		
-		
+					
 		four = new JButton("4");
 		add(four, "cell 3 4,aligny center");
-		four.setEnabled(false);
-		
+				
 		five = new JButton("5");
 		add(five, "cell 4 4,aligny center");
-		five.setEnabled(false);
-		
+				
 		six = new JButton("6");
 		add(six, "cell 5 4,aligny center");
-		six.setEnabled(false);
 		
 		seven = new JButton("7");
 		add(seven, "cell 6 4,aligny center");
-		seven.setEnabled(false);
 		
 		eight = new JButton("8");
 		add(eight, "cell 3 5,aligny center");
-		eight.setEnabled(false);
-		
+			
 		nine = new JButton("9");
 		add(nine, "cell 4 5,aligny center");
-		nine.setEnabled(false);
-		
+
 		ten = new JButton("A");
 		add(ten, "cell 5 5,aligny center");
-		ten.setEnabled(false);
 		
 		elvn = new JButton("B");
 		add(elvn, "cell 6 5,aligny center");
-		elvn.setEnabled(false);
 		
 		twelve = new JButton("C");
 		add(twelve, "cell 3 6,aligny center");
-		twelve.setEnabled(false);
 		
 		thrtn = new JButton("D");
 		add(thrtn, "cell 4 6,aligny center");
-		thrtn.setEnabled(false);
 		
 		fourtn = new JButton("E");
 		add(fourtn, "cell 5 6,aligny center");
-		fourtn.setEnabled(false);
 		
 		fiftn = new JButton("F");
 		add(fiftn, "cell 6 6,aligny center");
-		fiftn.setEnabled(false);
 		
+		//initialize/fill array list for ease of access/modification of state
+		num = new ArrayList<JButton>(){ //
+			{
+			add(zero);
+			add(one);
+			add(two);
+			add(three);
+			add(four);
+			add(five);
+			add(six);
+			add(seven);
+			add(eight);
+			add(nine);
+			add(ten);
+			add(elvn);
+			add(twelve);
+			add(thrtn);
+			add(fourtn);
+			add(fiftn);
+			}
+			};
 		
+			//adding actoinListeners to each of the buttons
+			for(JButton button : num)
+			{
+				button.addActionListener(digit);
+			}
+		
+	
 		//creation of operand buttons
 				clear = new JButton("C");//clear button gets its own listener
 				clear.addActionListener(clr);
@@ -195,6 +171,207 @@ public class Base4Panel extends JPanel {
 				equals = new JButton("=");
 				equals.addActionListener(operand);
 				add(equals, "cell 5 7,alignx left,aligny center");
+				
+				
+				//Creation of Jslider
+				slider = new JSlider();
+				slider.setMinorTickSpacing(1);
+				slider.setSnapToTicks(true);
+				slider.setPaintLabels(true);
+				slider.setMajorTickSpacing(2);
+				slider.setMinimum(2);
+				slider.setMaximum(16);
+					slider.addChangeListener(new ChangeListener() {
+					     public void stateChanged(ChangeEvent e) {
+					    JSlider source = (JSlider)e.getSource();
+					      if (!source.getValueIsAdjusting())
+					      {
+					    	
+					      if (slider.getValue()==2)
+					      {
+					    	  calc.setBase(2);
+					    	  System.out.print("calculating in base 2");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<2)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      else if(slider.getValue()==3)
+					      {
+					    	  calc.setBase(3);
+					    	  System.out.print("calculating in base 3");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<3)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					     
+					      else if(slider.getValue()==4)
+					      {
+					    	  calc.setBase(4);
+					    	  System.out.print("calculating in base ");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<4)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      
+					      if (slider.getValue()==5)
+					      {
+					    	  calc.setBase(5);
+					    	  System.out.print("calculating in base 5");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<5)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }  
+					      
+					      if (slider.getValue()==6)
+					      {
+					    	  calc.setBase(6);
+					    	  System.out.print("calculating in base 6");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<6)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      
+					      if (slider.getValue()==7)
+					      {
+					    	  calc.setBase(7);
+					    	  System.out.print("calculating in base 7");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<7)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      
+					      if (slider.getValue()==8)
+					      {
+					    	  calc.setBase(8);
+					    	  System.out.print("calculating in base 8");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<8)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      
+					      if (slider.getValue()==9)
+					      {
+					    	  calc.setBase(9);
+					    	  System.out.print("calculating in base 9");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<9)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      
+					      else if(slider.getValue()==10)
+					      {
+					    	  calc.setBase(10);
+					    	  System.out.print("calculating in base 10");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<10)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      if (slider.getValue()==11)
+					      {
+					    	  calc.setBase(11);
+					    	  System.out.print("calculating in base 11");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<11)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      
+					      if (slider.getValue()==12)
+					      {
+					    	  calc.setBase(12);
+					    	  System.out.print("calculating in base 12");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<12)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      
+					      if (slider.getValue()==13)
+					      {
+					    	  calc.setBase(13);
+					    	  System.out.print("calculating in base 13");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<13)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      
+					      if (slider.getValue()==14)
+					      {
+					    	  calc.setBase(14);
+					    	  System.out.print("calculating in base 14");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<14)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      
+					      if (slider.getValue()==15)
+					      {
+					    	  calc.setBase(15);
+					    	  System.out.print("calculating in base 15");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<15)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      else if(slider.getValue()==16)
+					      {
+					    	  calc.setBase(16);
+					    	  System.out.print("calculating in base 16");
+					    	  for (int i=0; i < num.size(); i++)
+					    		  if (i<16)
+					    		  (num.get(i)).setEnabled(true);
+					    		  else
+					    		  (num.get(i)).setEnabled(false);
+					    			  					      
+					      }
+					      
+					      
+					      }
+					     } 
+					    });
+						
+						
+				slider.setOrientation(SwingConstants.VERTICAL);
+				add(slider, "cell 0 2 2 5");
 			
 	
 	}
